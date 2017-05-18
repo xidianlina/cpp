3677d0028b75374c149aafee5d3f317d9723dd07
xidianlina <1198827599@qq.com>
2017-05-18 22:18:44 +0800

    chapter 12

---
+++ twelve_stringbad.h
@@ -0,0 +1,18 @@
+//#pragma once
+//#ifndef STRINGBAD_H_
+//#define STRINGBAD_H_
+//#include <iostream>
+//
+//class StringBad
+//{
+//private:
+//	char *str;
+//	int len;
+//	static int num_strings;
+//public:
+//	StringBad(const char *s);
+//	StringBad();
+//	~StringBad();
+//	friend std::ostream &operator<<(std::ostream &os, const StringBad &st);
+//};
+//#endif
