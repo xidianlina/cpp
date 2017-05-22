c0f821d43805c3d69d0a2cd275323dc47f5badae
xidianlina <1198827599@qq.com>
2017-05-22 19:44:09 +0800

    chapter 13 first

---
+++ thirteen_tabtenn0.cpp
@@ -0,0 +1,8 @@
+//#include <iostream>
+//#include "thirteen_tabtenn0.h"
+//
+//TableTennisPlayer::TableTennisPlayer(const string &fn, const string &ln, bool ht) :firstname(fn), lastname(ln), hasTable(ht) {}
+//void TableTennisPlayer::Name()const
+//{
+//	std::cout << lastname << ", " << firstname;
+//}
